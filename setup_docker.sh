f4fe423283d656df1ed8bb4a7ac6a1c86b285362
roger-lab-01 <Evaluate9933@proton.me>
2026-02-28 09:33:08 +0700

    Update setup_docker.sh

--- setup_docker.sh
+++ setup_docker.sh
@@ -22,7 +22,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # 4. Set up the repository
 echo \
-  "座eb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
